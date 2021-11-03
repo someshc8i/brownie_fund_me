acab7c561ed4d82c91a6074356950bc6aad0d871
Somesh <somesh@mudrex.com>
2021-11-03 13:30:13 +0530

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,23 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, deploy_mocks, get_account
+
+
+def deploy_fund_me():
+    account = get_account()
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config['networks'][network.show_active(
+        )]['eth_usd_price_feed']
+    else:
+        print(f"The active network is {network.show_active()}")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+        print('Mocks deployed')
+
+    fund_me = FundMe.deploy(
+        price_feed_address, {'from': account}, publish_source=config['networks'][network.show_active()].get('verify', False))
+    print(f"Contract deployed to {fund_me.address}")
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
